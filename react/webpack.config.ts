56a7178b8f3d2a311416da8f5f262596b702a68c
최은기 <galaxyhi4276@gmail.com>
2021-08-09 11:45:45 +0900

    feat: 누락된 웹팩 설정 작성

---
+++ react/webpack.config.ts
@@ -0,0 +1,51 @@
+import { join } from 'path';
+import { Configuration } from 'webpack';
+// @ts-ignore TODO: esModuleInterop Lint 적용이 안 됨
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+type DevMode = 'production' | 'development' | 'none';
+
+const WebpackConfig: Configuration = {
+  name: 'react-typescript-webpack-config',
+  mode: process.env.NODE_ENV as DevMode | 'development',
+  entry: join(__dirname, "src", "App", "index.tsx"),
+  output: {
+    path: join(__dirname, "build"),
+    filename: "index.bundle.js"
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  devServer: {
+    contentBase: join(__dirname, "./build/"),
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+    port: 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["swc-loader"]
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: join(__dirname, "src", "index.html"),
+    }),
+  ]
+};
+
+export default WebpackConfig;
