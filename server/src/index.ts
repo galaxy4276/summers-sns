9158f44528956012c82f466b90cee0c275f3bc01
최은기 <galaxyhi4276@gmail.com>
2021-08-10 13:43:27 +0900

    feat: 기존 코드 개선

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,14 +2,11 @@ import getKoaServer from '@config/application';
 import { debug as log } from 'loglevel';
 import conn from '@config/database';
 
-const runMethodCallback = () =>
-  log(`listening on ${process.env.SERVER_PORT}...`);
-
 async function main(): Promise<void> {
   const koa = getKoaServer();
   await conn.connect().then(() => log('Database Connected'));
   await conn.initialize();
-  koa.run(runMethodCallback);
+  koa.run(() => log(`listening on ${process.env.SERVER_PORT}...`));
 }
 
 main();
