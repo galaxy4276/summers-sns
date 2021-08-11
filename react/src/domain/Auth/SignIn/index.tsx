e0ac484329131a01907fbb29a02a683171afd4ee
최은기 <galaxyhi4276@gmail.com>
2021-08-11 22:11:31 +0900

    feat: 페이지 초안 작성

---
+++ react/src/domain/Auth/SignIn/index.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react';
+
+function SignIn(): ReactElement {
+	return (
+		<>
+			<div>방구</div>
+		</>
+	);
+}
+
+export default SignIn;
---
+++ react/src/domain/Main/index.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react';
+
+function MainPage(): ReactElement {
+	return (
+		<>
+			<div>홈</div>
+		</>
+	);
+}
+
+export default MainPage;
