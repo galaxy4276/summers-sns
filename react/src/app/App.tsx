fd67686ac8f4d76ca0492d71770ad6cfebae043a
최은기 <galaxyhi4276@gmail.com>
2021-08-11 14:18:15 +0900

    fix: App git 이상현상 커밋

--- react/src/app/App.tsx
+++ react/src/app/App.tsx
@@ -1,15 +1,7 @@
 import React, { ReactElement } from 'react';
-import { RecoilRoot } from 'recoil';
-import { ChakraProvider } from '@chakra-ui/react';
 
 function App(): ReactElement {
-  return (
-    <ChakraProvider>
-      <RecoilRoot>
-        <div>hello</div>
-      </RecoilRoot>
-    </ChakraProvider>
-  );
+  return <div>안녕</div>;
 }
 
 export default App;
