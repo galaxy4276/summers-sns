f217ad7b7aa746828bba5763b9ff9963fdee0ce7
최은기 <galaxyhi4276@gmail.com>
2021-08-11 14:18:45 +0900

    feat: 환경 스크립트 개선, react-router 설치

--- react/webpack.config.ts
+++ react/webpack.config.ts
@@ -1,4 +1,4 @@
-import { join } from 'path';
+import { join, resolve } from 'path';
 import { Configuration } from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
@@ -10,16 +10,16 @@ const WebpackConfig: Configuration = {
   entry: join(__dirname, "src", "app", "index.tsx"),
   output: {
     path: join(__dirname, "build"),
-    filename: "index.bundle.js"
+    filename: "index.bundle.js",
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
     alias: {
-      "@api": "./src/api/",
-      "@config": "./src/config/",
-      "@middlewares": "./src/middlewares/",
-      "@services": "./src/services/",
-      "@test": "./test"
+      "@app": resolve(__dirname, 'src/app/'),
+      "@config": resolve(__dirname, '/src/config/'),
+      "@middlewares": resolve(__dirname, '/src/middlewares/'),
+      "@services": resolve(__dirname, '/src/services/'),
+      "@test": resolve(__dirname, '/test/')
     },
   },
   devServer: {
