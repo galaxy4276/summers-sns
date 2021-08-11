6576dc334da56f074e77527b9d345f182be20c19
최은기 <galaxyhi4276@gmail.com>
2021-08-11 22:22:49 +0900

    feat: theme 수정

--- react/src/App/Theme/index.tsx
+++ react/src/App/Theme/index.tsx
@@ -1,18 +1,24 @@
 import { extendTheme } from '@chakra-ui/react';
 
-const index = extendTheme({
-  global: {
-    body: {
-      bg: 'gray.100',
-    },
-  },
-  colors: {
-    sadgray: '#FAFAFA',
-    happyblue: {
-      0: '#9DC9CC',
-      1: '#3FC1C9',
-    },
-  },
+const theme = extendTheme({
+	styles: {
+		global: {
+			backgroundColor: 'sadgray',
+			html: {
+				bg: 'gray.100',
+			},
+			body: {
+				bg: 'gray.100',
+			},
+		},
+	},
+	colors: {
+		sadgray: '#FAFAFA',
+		happyblue: {
+			0: '#9DC9CC',
+			1: '#3FC1C9',
+		},
+	},
 });
 
-export default index;
+export default theme;
