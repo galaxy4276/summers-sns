19eabc2627b0d14306aaccdfbca3f3f48ac40fd2
최은기 <galaxyhi4276@gmail.com>
2021-08-11 22:10:50 +0900

    feat: App 환경설정 컴포넌트 분리 및 작성

--- react/src/app/App.tsx
+++ react/src/app/App.tsx
@@ -2,18 +2,19 @@ import React, { ReactElement } from 'react';
 import { RecoilRoot as Recoil } from 'recoil';
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import index from '@app/Theme';
+import theme from '@app/Theme';
+import RouterSwitch from '@app/RouterSwitch';
 
 function App(): ReactElement {
-  return (
-    <Router>
-      <ChakraProvider theme={index}>
-        <Recoil>
-          <div>hello</div>
-        </Recoil>
-      </ChakraProvider>
-    </Router>
-  );
+	return (
+		<Router>
+			<ChakraProvider theme={theme}>
+				<Recoil>
+					<RouterSwitch />
+				</Recoil>
+			</ChakraProvider>
+		</Router>
+	);
 }
 
 export default App;
