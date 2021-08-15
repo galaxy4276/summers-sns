c962202184c2aa5e402f8fcc42bcb14062e3ff52
최은기 <galaxyhi4276@gmail.com>
2021-08-15 20:04:41 +0900

    feat: 메인 회원가입 로직 email, phone 로직 분기

--- server/src/api/user/user.controller.ts
+++ server/src/api/user/user.controller.ts
@@ -1,35 +1,69 @@
 import { Context, Next } from 'koa';
-import { validateSignInForm } from '@api/user/schema';
-import { SignInProps } from '@typings/user';
-import { checkPrevUser, createUser } from '@api/user/sql';
+import {
+  isSignInEmailForm,
+  isSignInPhoneForm,
+  validateSignInFormEmail,
+  validateSignInFormPhone,
+} from '@api/user/validate';
+import {
+  checkPrevUserByEmail,
+  checkPrevUserByPhone,
+  createUserByEmail,
+  createUserByPhone,
+} from '@api/user/sql';
 import { hashPlainText } from '@services/index';
 
+/**
+ * @desc 사용자를 검증하고 회원가입 로직을 수행하는 컨트롤러
+ */
 export const signInController = async (
   ctx: Context,
   next: Next,
 ): Promise<void> => {
   try {
-    const signInForm: SignInProps = ctx.request.body;
-    validateSignInForm(signInForm);
-    const { username, email, password: plainPassword } = signInForm;
-    const errMessage = await checkPrevUser(username, email);
-    if (errMessage) {
-      ctx.response.status = 400;
-      ctx.body = {
-        message: errMessage,
-      };
-      return await next();
+    const signInForm = ctx.request.body;
+    const isEmail = isSignInEmailForm(signInForm);
+    const isPhone = isSignInPhoneForm(signInForm);
+    if (isEmail) {
+      const { username, email } = validateSignInFormEmail(signInForm);
+      const emailError = await checkPrevUserByEmail(username, email);
+      if (emailError) {
+        ctx.body = {
+          message: emailError,
+        };
+        return await next();
+      }
     }
-    const password = await hashPlainText(plainPassword, 10);
+    if (isPhone) {
+      const { username, phone } = validateSignInFormPhone(signInForm);
+      const phoneError = await checkPrevUserByPhone(username, phone);
+      console.log({ phoneError });
+      if (phoneError) {
+        ctx.body = {
+          message: phoneError,
+        };
+        return await next();
+      }
+    }
+
+    const password = await hashPlainText(signInForm.password, 10);
     const form = {
       ...signInForm,
       password,
     };
-    const createdUser = await createUser(form);
+    if (isEmail) {
+      const createdUser = await createUserByEmail(form);
+      ctx.body = {
+        message: `${createdUser.username} 유저가 생성되었습니다.`,
+      };
+    } else if (isPhone) {
+      const createdUser = await createUserByPhone(form);
+      ctx.body = {
+        message: `${createdUser.username} 유저가 생성되었습니다.`,
+      };
+    }
     ctx.response.status = 201;
-    ctx.body = {
-      message: `${createdUser?.username} 유저가 생성되었습니다.`,
-    };
+    await next();
   } catch (err) {
     if (err.message.includes('joi')) {
       ctx.response.status = 400;
