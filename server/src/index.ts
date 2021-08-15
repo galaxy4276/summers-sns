0f030141973a6a221f90c0d4cb3881cbdf56bafe
galaxy4276 <galaxyhi4276@gmail.com>
2021-08-16 00:17:50 +0900

    feat: 폰 번호, 이메일 회원가입 분기 처리 (#21)
    * feat: 기존 sql 관련 함수 phone, email 로 분기 작성
    
    * feat: 타입 분기 작성
    
    * fix: 디자인 패턴이 변경되면서 기존 테스트 파일 오류 수
    
    * feat: 이전 브랜치 누락사항 반영
    
    * feat: 메인 회원가입 로직 email, phone 로직 분기
    
    * feat: session 미들웨어 적용
    
    * feat: 변경된 회원가입에 따른 테스트 케이스 추가 및 테스트 작성 중 오류 사항 수정

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,6 +14,7 @@ async function main(): Promise<void> {
     .setSwaggerUi()
     .setLogger()
     .setParser()
+    .setAuthMiddlewares()
     .setRouter()
     .setErrorHandler(errorCb);
   await conn.initialize();
