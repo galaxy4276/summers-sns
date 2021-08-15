75c6a6089c4230286ee9b5ea4def7bc343282ca5
최은기 <galaxyhi4276@gmail.com>
2021-08-15 20:04:23 +0900

    feat: 이전 브랜치 누락사항 반영

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,10 +1,12 @@
 import Router from 'koa-router';
 import userRouter from '@api/user/user.routes';
+import testRouter from '@api/test/test.routes';
 
 const rootRouter = new Router({ prefix: '/api' });
 
 rootRouter.get('/', () => ({ test: true }));
 
 rootRouter.use(userRouter.routes());
+rootRouter.use(testRouter.routes());
 
 export default rootRouter;
