ebdc1c33805c503ae638043a25dd89fa126f5657
최은기 <galaxyhi4276@gmail.com>
2021-08-17 02:26:07 +0900

    feat: 보안 코드 전송 컨트롤러 작성 및 라우터 적용

--- server/src/api/user/routes.ts
+++ server/src/api/user/routes.ts
@@ -1,9 +1,14 @@
 import Router from 'koa-router';
-import { createUserRoleController, signInController } from './controller';
+import {
+  createUserRoleController,
+  sendSecurityCodeController,
+  signInController,
+} from './controller';
 
 const userRouter = new Router({ prefix: '/user' });
 
 userRouter.post('/role', createUserRoleController);
+userRouter.post('/sms', sendSecurityCodeController);
 userRouter.post('/', signInController);
 
 export default userRouter;
