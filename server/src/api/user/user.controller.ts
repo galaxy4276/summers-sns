ce066163e65553f4f726e7db92644044b9cd9152
최은기 <galaxyhi4276@gmail.com>
2021-08-16 20:22:16 +0900

    feat: 기존 로직 리팩터링 및 사용자 정보 생성 컨트롤러 작성

--- server/src/api/user/user.controller.ts
+++ server/src/api/user/user.controller.ts
@@ -1,17 +1,11 @@
 import { Context, Next } from 'koa';
 import {
-  checkPrevUserByEmail,
-  checkPrevUserByPhone,
   createUserByEmail,
   createUserByPhone,
+  createUserRole,
 } from '@api/user/sql';
 import { hashPlainText } from '@services/index';
-import {
-  isSignInEmailForm,
-  isSignInPhoneForm,
-  validateSignInFormEmail,
-  validateSignInFormPhone,
-} from './validate';
+import { validateUser, validateUserRole } from './validate';
 
 /**
  * @desc 사용자를 검증하고 회원가입 로직을 수행하는 컨트롤러
@@ -22,30 +16,8 @@ export const signInController = async (
 ): Promise<void> => {
   try {
     const signInForm = ctx.request.body;
-    const isEmail = isSignInEmailForm(signInForm);
-    const isPhone = isSignInPhoneForm(signInForm);
-    if (isEmail) {
-      const { username, email } = validateSignInFormEmail(signInForm);
-      const emailError = await checkPrevUserByEmail(username, email);
-      ctx.response.status = 400;
-      if (emailError) {
-        ctx.body = {
-          message: emailError,
-        };
-        return await next();
-      }
-    }
-    if (isPhone) {
-      const { username, phone } = validateSignInFormPhone(signInForm);
-      const phoneError = await checkPrevUserByPhone(username, phone);
-      ctx.response.status = 400;
-      if (phoneError) {
-        ctx.body = {
-          message: phoneError,
-        };
-        return await next();
-      }
-    }
+    const { isError, isEmail, isPhone } = await validateUser(ctx, signInForm);
+    if (isError) return await next();
 
     const password = await hashPlainText(signInForm.password, 10);
     const form = {
@@ -57,14 +29,15 @@ export const signInController = async (
       ctx.body = {
         message: `${createdUser.username} 유저가 생성되었습니다.`,
       };
-    } else if (isPhone) {
+    }
+    if (isPhone) {
       const createdUser = await createUserByPhone(form);
       ctx.body = {
         message: `${createdUser.username} 유저가 생성되었습니다.`,
       };
     }
     ctx.response.status = 201;
-    await next();
+    return await next();
   } catch (err) {
     if (err.message.includes('joi')) {
       ctx.response.status = 400;
@@ -77,4 +50,24 @@ export const signInController = async (
   }
 };
 
-export default {};
+/**
+ * @desc 사용자 정보를 생성하는 컨트롤러
+ */
+export const createUserRoleController = async (
+  ctx: Context,
+  next: Next,
+): Promise<void> => {
+  const signInForm = ctx.request.body;
+  const isPrevious = await validateUserRole(ctx, signInForm);
+  if (isPrevious) return next();
+
+  const createdRoleId = await createUserRole(
+    signInForm.email,
+    signInForm.phone,
+  );
+
+  ctx.body = {
+    createdRoleId,
+  };
+  return next();
+};
