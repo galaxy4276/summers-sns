c7856fc7eb14577244efcf3a55528f498742d1cf
최은기 <galaxyhi4276@gmail.com>
2021-08-20 02:15:31 +0900

    feat: 로그인 상태 테스트를 위한 로직 개발

--- server/src/api/test/test.controller.ts
+++ server/src/api/test/test.controller.ts
@@ -4,8 +4,11 @@ import { inspect } from 'util';
 export const debugSessionController = (ctx: Context, next: Next): void => {
   const session = inspect(ctx.session);
   const cookie = inspect(ctx.cookies);
+  console.log(ctx.user);
+  const { user } = ctx;
   ctx.body = {
     session,
+    user,
     cookie,
   };
   next();
--- server/src/api/test/test.routes.ts
+++ server/src/api/test/test.routes.ts
@@ -1,8 +1,18 @@
 import Router from 'koa-router';
 import { debugSessionController } from '@api/test/test.controller';
+import { Session } from 'koa-session';
 
 const testRouter = new Router({ prefix: '/test' });
 
 testRouter.get('/session', debugSessionController);
+testRouter.get('/session/views', (ctx) => {
+  const session = <Session>ctx.session;
+  const n = session.views || 0;
+  session.views += n;
+  ctx.body = {
+    message: `${n} views`,
+    n,
+  };
+});
 
 export default testRouter;
