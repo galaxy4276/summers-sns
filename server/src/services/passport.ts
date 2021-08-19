631b9ebcb4c528f7c56d6ef54915921ba768f10d
최은기 <galaxyhi4276@gmail.com>
2021-08-20 02:15:54 +0900

    feat: 1차 로그인 로직 작성

--- server/src/services/passport.ts
+++ server/src/services/passport.ts
@@ -1,22 +1,135 @@
-import { config } from 'dotenv';
-import { Context } from 'koa';
+import { Context, Next } from 'koa';
+import { mariadb } from '@config/index';
+import { getUserByEmail, getUserByPhone } from '@test/auth/clear-database';
+import { Session } from 'koa-session';
+import { v4 as uuid } from 'uuid';
+import { compareHash } from '@services/bcrypt';
+import { Connection } from 'mariadb';
+import { getUserById } from '@services/common-sql';
 
 class LocalPassPort {
-  private readonly key: string;
-
-  constructor() {
-    config();
-    this.key = process.env.SESSION_KEY as string;
+  setNoUserError(ctx: Context): void {
+    ctx.body = {
+      message: '사용자가 존재하지 않습니다.',
+    };
+    ctx.response.status = 401;
   }
 
+  setPasswordError(ctx: Context): void {
+    ctx.body = {
+      message: '비밀번호가 일치하지 않습니다.',
+    };
+    ctx.response.status = 400;
+  }
+
+  createSessionId(): string {
+    return uuid();
+  }
+
+  async saveSessionDatabase(
+    conn: Connection,
+    sessionId: string,
+    userId: number,
+  ): Promise<number> {
+    const { insertId }: { insertId: number } = await conn.query(
+      `
+      INSERT INTO \`summers-sns\`.session_store (session_id, user_id) VALUES (?, ?);
+    `,
+      [sessionId, userId],
+    );
+    return insertId;
+  }
+
+  async getSessionDatabase(sessionId: string): Promise<any> {
+    const conn = await mariadb.pool.getConnection();
+    const queryResult = await conn.query(
+      `SELECT session_id FROM \`summers-sns\`.session_store WHERE session_id = ?;`,
+      [sessionId],
+    );
+    return queryResult[0];
+  }
+
+  // TODO: Need Refactor
   authenticate() {
-    return (ctx: Context) => {
+    return async (ctx: Context, next: Next) => {
       try {
-        const { email, password } = ctx.request.body;
-        console.log(email, password, this.key);
+        const conn = await mariadb.pool.getConnection();
+        const { email, phone, password } = ctx.request.body;
+        const session = <Session>ctx.session;
+        if (email) {
+          const user = await getUserByEmail(conn, email);
+          if (!user?.id) {
+            this.setNoUserError(ctx);
+            await conn.end();
+            return await next();
+          }
+          const isCorrectPassword = await compareHash(password, user.password);
+          if (isCorrectPassword) {
+            const sessionId = this.createSessionId();
+            await this.saveSessionDatabase(conn, sessionId, user.id);
+            session[sessionId] = user.id;
+          } else {
+            this.setPasswordError(ctx);
+          }
+          await next();
+        }
+        if (phone) {
+          const user = await getUserByPhone(conn, phone);
+          if (!user?.id) {
+            this.setNoUserError(ctx);
+            await conn.end();
+            return await next();
+          }
+          const isCorrectPassword = await compareHash(password, user.password);
+          if (isCorrectPassword) {
+            const sessionId = this.createSessionId();
+            console.log('세션 데이터베이스 작업');
+            await this.saveSessionDatabase(conn, sessionId, user.id);
+            ctx.cookies.set('sid', sessionId);
+            session[sessionId] = user.id;
+          }
+          if (!isCorrectPassword) {
+            this.setPasswordError(ctx);
+          }
+        }
+
+        ctx.response.status = 200;
+        await conn.end();
+        await next();
       } catch (err) {
         throw new Error(err);
       }
+    };
+  }
+
+  async getDbSessionIfNotExists(ctx: Context, clientId: string): Promise<any> {
+    const session = <Session>ctx.session;
+    console.log({ session });
+    const serverSession = session[clientId] as string | undefined;
+    if (serverSession) {
+      console.log('서버 로컬 세션 반환');
+      return serverSession;
+    }
+    const sessionData = (await this.getSessionDatabase(clientId)) as any;
+    console.log({ sessionData });
+    return sessionData;
+  }
+
+  session() {
+    return async (ctx: Context, next: Next) => {
+      console.log('session 미들웨어 시작');
+      if (ctx.path === '/api/user/login') {
+        return next();
+      }
+      const clientSessionId = ctx.cookies.get('sid');
+      if (!clientSessionId) return next();
+      console.log({ clientSessionId });
+      const test = await this.getDbSessionIfNotExists(ctx, clientSessionId);
+      console.log({ test });
+      // FIXME: 데이터베이스에서 꺼내올 경우도 추가해야한다.
+      const userId = await this.getDbSessionIfNotExists(ctx, clientSessionId);
+      if (userId) ctx.user = await getUserById(userId);
+      await next();
     };
   }
 }
