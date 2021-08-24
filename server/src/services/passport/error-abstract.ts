ce8ed0a2df7b62f75dab7396707fe8a8c1f60a63
최은기 <galaxyhi4276@gmail.com>
2021-08-24 14:46:50 +0900

    feat: 인증 관련 에러 추상 클래스 작성

---
+++ server/src/services/passport/error-abstract.ts
@@ -0,0 +1,44 @@
+import { Context } from 'koa';
+
+/**
+ * @desc 에러 관련 설정을 포함한 인증 관련 추상 클래스입니다.
+ */
+export default class AuthErrorAbstract {
+  setNoUserError(ctx: Context): void {
+    ctx.body = {
+      message: '사용자가 존재하지 않습니다.',
+    };
+    ctx.response.status = 401;
+  }
+
+  setPasswordError(ctx: Context): void {
+    ctx.body = {
+      message: '비밀번호가 일치하지 않습니다.',
+    };
+    ctx.response.status = 400;
+  }
+
+  setNoPasswordError(ctx: Context): void {
+    ctx.body = {
+      message: '비밀번호가 입력되지 않았습니다.',
+    };
+    ctx.response.status = 400;
+  }
+
+  setAuthenticationFaultError(ctx: Context): void {
+    ctx.response.status = 400;
+    ctx.body = { message: '요청이 잘못되었습니다.' };
+  }
+
+  setServerError(ctx: Context): void {
+    ctx.response.status = 500;
+    ctx.body = { message: '서버에서 에러가 발생하였습니다.' };
+  }
+
+  setNoAuthenticationError(ctx: Context): void {
+    ctx.body = {
+      message: '로그인 되어 있지 않은 유저입니다.',
+    };
+    ctx.response.status = 400;
+  }
+}
