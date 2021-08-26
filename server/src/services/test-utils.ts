1432684f30005f46a7c8ea1adc828cd15ba95e27
최은기 <galaxyhi4276@gmail.com>
2021-08-27 04:07:38 +0900

    refactor: 기존 테스트 관련 유틸 함수 개선

--- server/src/services/test-utils.ts
+++ server/src/services/test-utils.ts
@@ -1,16 +1,23 @@
 import { Context } from 'koa';
 
+/**
+ * @desc 테스트 요청 정보가 포함되어 있는 지 확인합니다.
+ * message & statusCode 가 포함되어 있는 경우 body 와 response code 를 설정합니다.
+ * @return boolean
+ */
 export function isTestProps(
   ctx: Context,
-  message: string,
-  statusCode: number,
+  message?: string,
+  statusCode?: number,
 ): boolean {
   const { isTest } = ctx.request.body;
   if (isTest) {
-    ctx.body = {
-      message,
-    };
-    ctx.response.status = statusCode;
+    if (message && statusCode) {
+      ctx.body = {
+        message,
+      };
+      ctx.response.status = statusCode;
+    }
     return true;
   }
   return false;
